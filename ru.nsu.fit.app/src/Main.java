c9c7fb1c79a0f91f4c83259c8eb66599fdc5a0d2
Matvey0123 <m.kuznetsov4@g.nsu.ru>
2021-12-01 23:31:41 +0700

    dev_initial_commit_with_prolog_connection

---
+++ ru.nsu.fit.app/src/Main.java
@@ -0,0 +1,17 @@
+import org.projog.api.Projog;
+import org.projog.api.QueryResult;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        Projog projog = new Projog();
+        projog.consultFile(new File("test.pl"));
+        QueryResult result = projog.executeQuery("test(X, Y).");
+        while (result.next()) {
+            System.out.println("X = " + result.getTerm("X") + " Y = " + result.getTerm("Y"));
+        }
+
+
+    }
+}
